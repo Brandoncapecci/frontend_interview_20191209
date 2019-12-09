d4247575401106619bcd3e182cfb25a47cf2dfac
Brandon Capecci <brandoncapecci@Brandons-MacBook-Pro.local>
2019-12-09 15:28:45 -0500

    Inital commit

---
+++ src/pages/Firmware/FirmwarePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FirmwarePage.scss';
+
+function FirmwarePage() {
+  return (
+    <div className="firmware-page">
+      <h1>Firmware Page</h1>
+    </div>
+  );
+}
+
+export default FirmwarePage;
